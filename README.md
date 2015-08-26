c31216fbc1d709d602364245903d4b5ad69f3364
Spencer Elliott <me@elliottsj.com>
2015-08-25 20:10:23 -0400

    Enable hot loading via `npm run hot`

--- README.md
+++ README.md
@@ -5,7 +5,10 @@ Example React Native project using react-native-webpack-server
 
 ```shell
 npm install
+
 npm start
+# OR with hot loading enabled:
+npm run hot
 ```
 
 Then open **Example.xcodeproj** and <kbd>Cmd</kbd>+<kbd>R</kbd>
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,9 @@
 'use strict'
 
 var path = require('path')
+var webpack = require('webpack')
 
-module.exports = {
+var config = {
   debug: true,
   devtool: 'source-map',
   entry: './src/index.js',
@@ -16,12 +17,24 @@ module.exports = {
       {
         test: /\.js$/,
         include: path.resolve(__dirname, 'src'),
-        loader: 'babel-loader',
-        query: {
-          stage: 0,
-          blacklist: 'validation.react'
-        }
+        loaders: ['babel-loader?stage=0,blacklist=validation.react']
       }
     ]
   }
 }
+
+// Hot loader
+if (process.env.HOT) {
+  config.devtool = 'eval' // Speed up incremental builds
+  config.entry = [
+    'react-native-webpack-server/hot/entry', 
+    'webpack/hot/only-dev-server',
+    'webpack-dev-server/client?http://localhost:8082'
+  ].concat(config.entry)
+  config.output.publicPath = 'http://localhost:8082/'
+  config.module.loaders[0].loaders.unshift('react-hot')
+  config.plugins = config.plugins || []
+  config.plugins.unshift(new webpack.HotModuleReplacementPlugin())
+}
+
+module.exports = config
